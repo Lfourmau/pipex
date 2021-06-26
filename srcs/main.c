17613be1d3a37b93e881b3afc5d7259a71454b6e
Loic Fourmaux <Loicfourmaux@gmail.com>
2021-06-26 11:47:38 +0200

    first test to discover fork

--- srcs/main.c
+++ srcs/main.c
@@ -2,5 +2,17 @@
 
 int main(int argc, char **argv)
 {
+    pid_t pid;
+
+    printf("AVANT FORK\n");
+    pid = fork();
+    if (pid == 0)
+        printf("APRES FORK PROCESS FILS\n");
+    else
+    {
+        printf("APRES FORK PROCESS PAPS\n");
+        wait(NULL);
+    }
+    printf("FIN FORK\n");
     
 }
