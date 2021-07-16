5df65c79287275c02f7afaf5bbbe2b19c8d983a2
Loic Fourmaux <lfourmau@z4r7p3.42lyon.fr>
2021-07-16 14:44:55 +0200

    tests avec execution d'un scirpt via le main

--- srcs/main.c
+++ srcs/main.c
@@ -4,32 +4,22 @@ int main(int argc, char **argv)
 {
 	pid_t   pid;
 	int     pipefd[2];
-	int		dup;
+	FILE	*fd;
 	int     read_return;
 	char    buffer[11];
 
 	if (pipe(pipefd) < 0)
 		return (1);
-	dup2(pipefd[0], dup);
 	pid = fork();
 	if (pid != 0)
 	{
-		close (pipefd[0]);
-		printf("dad\n");
-		write (pipefd[1], "Bonjour #1", 11);
-		write (pipefd[1], "Bonjour #2", 11);
-		write (pipefd[1], "Bonjour #3", 11);
-		execve("./test", NULL, NULL);
+		printf("daddy\n");
+		fd = fopen("meuh.sh", "rb+");
+		execve("./meuh.sh", NULL, NULL);
 		wait(NULL);
 	}
 	else
 	{
-		close(pipefd[1]);
-		printf("child\n");
-		while ((read_return = read(dup, buffer, 11)) > 0)
-			printf("%s\n", buffer);
-		if (read_return != 0)  
-			return (1);
-		printf("Finish le fish\n");
+		printf("son\n");
 	}
 }
--- srcs/test.c
+++ srcs/test.c
@@ -5,5 +5,5 @@ int main(int argc, char **argv)
 	int i = -1;
 
     while (++i <= 5)
-        printf("Edouard est une immense zoulette n'%d\n", i);
+        printf("Edouard est une immense zoul n'%d\n", i);
 }
