f6f18a588520e61c1fde4414b0c134b93e9d1a69
Loic Fourmaux <lfourmau@z4r7p3.42lyon.fr>
2021-07-16 08:05:38 +0200

    add pipe in test. Discover fork + pipe

--- srcs/main.c
+++ srcs/main.c
@@ -2,21 +2,31 @@
 
 int main(int argc, char **argv)
 {
-    pid_t pid;
+	pid_t   pid;
+	int     pipefd[2];
+	int     read_return;
+	char    buffer[11];
 
-    printf("AVANT FORK\n");
-    pid = fork();
-    if (pid == 0)
-        printf("APRES FORK PROCESS FILS\n");
-    else
-    {
-        printf("APRES FORK PROCESS PAPS\n");
-        wait(NULL);
-    }
-    if (pid == 0)
-        printf("FIN PROCESS PAPS\n");
-    else
-        printf("FIN PROCESS FILS\n");
-
-    
+	if (pipe(pipefd) < 0)
+		return (1);
+	pid = fork();
+	if (pid != 0)
+	{
+		close (pipefd[0]);
+		printf("dad\n");
+		write (pipefd[1], "Bonjour #1", 11);
+		write (pipefd[1], "Bonjour #2", 11);
+		write (pipefd[1], "Bonjour #3", 11);
+		wait(NULL);
+	}
+	else
+	{
+		close(pipefd[1]);
+		printf("child\n");
+		while ((read_return = read(pipefd[0], buffer, 11)) > 0)
+			printf("%s\n", buffer);
+		if (read_return != 0)  
+			return (1);
+		printf("Finish le fish\n");
+	}
 }
