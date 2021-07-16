14251ca77f241588bb18c53e10a3256a784f3100
Loic Fourmaux <lfourmau@z4r7p3.42lyon.fr>
2021-07-16 09:13:44 +0200

    pipe, dup and execve. c magik

--- srcs/main.c
+++ srcs/main.c
@@ -4,11 +4,13 @@ int main(int argc, char **argv)
 {
 	pid_t   pid;
 	int     pipefd[2];
+	int		dup;
 	int     read_return;
 	char    buffer[11];
 
 	if (pipe(pipefd) < 0)
 		return (1);
+	dup2(pipefd[0], dup);
 	pid = fork();
 	if (pid != 0)
 	{
@@ -17,13 +19,14 @@ int main(int argc, char **argv)
 		write (pipefd[1], "Bonjour #1", 11);
 		write (pipefd[1], "Bonjour #2", 11);
 		write (pipefd[1], "Bonjour #3", 11);
+		execve("./test", NULL, NULL);
 		wait(NULL);
 	}
 	else
 	{
 		close(pipefd[1]);
 		printf("child\n");
-		while ((read_return = read(pipefd[0], buffer, 11)) > 0)
+		while ((read_return = read(dup, buffer, 11)) > 0)
 			printf("%s\n", buffer);
 		if (read_return != 0)  
 			return (1);
---
+++ srcs/test.c
@@ -0,0 +1,9 @@
+#include "../includes/pipex.h"
+
+int main(int argc, char **argv)
+{
+	int i = -1;
+
+    while (++i <= 5)
+        printf("Edouard est une immense zoulette n'%d\n", i);
+}
