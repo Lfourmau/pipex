264cc9712dccfac8cd4b01f98bc7ad2085bdb515
Loic Fourmaux <lfourmau@z4r2p1.42lyon.fr>
2021-07-20 11:40:56 +0200

    fopen fixed. Permission bug fixed.

--- srcs/create_cmd_path.c
+++ srcs/create_cmd_path.c
@@ -20,19 +20,19 @@ static char	*create_cmd_util(char **cmd)
 	char	*path;
 
 	path = ft_strjoin("/usr/local/bin/", cmd[0]);
-	if (fopen(path, "r"))
+	if (open(path, O_RDONLY) != -1)
 		return (ret_path(path, cmd, 0));
 	free(path);
 	path = ft_strjoin("/usr/bin/", cmd[0]);
-	if (fopen(path, "r"))
+	if (open(path, O_RDONLY) != -1)
 		return (ret_path(path, cmd, 0));
 	free(path);
 	path = ft_strjoin("/bin/", cmd[0]);
-	if (fopen(path, "r"))
+	if (open(path, O_RDONLY) != -1)
 		return (ret_path(path, cmd, 0));
 	free(path);
 	path = ft_strjoin("/usr/sbin/", cmd[0]);
-	if (fopen(path, "r"))
+	if (open(path, O_RDONLY) != -1)
 		return (ret_path(path, cmd, 0));
 	return (ret_path(path, cmd, 1));
 }
@@ -41,18 +41,17 @@ char	*create_command_path(char *cmd)
 {
 	char	*path;
 	char	**words;
-
 	words = ft_split(cmd, ' ');
 	path = ft_strjoin("/sbin/", words[0]);
-	if (fopen(path, "r"))
+	if (open(path, O_RDONLY) != -1)
 		return (ret_path(path, words, 0));
 	free(path);
 	path = ft_strjoin("/usr/local/munki/", words[0]);
-	if (fopen(path, "r"))
+	if (open(path, O_RDONLY) != -1)
 		return (ret_path(path, words, 0));
 	free(path);
 	path = ft_strjoin("/opt/X11/bin/", words[0]);
-	if (fopen(path, "r"))
+	if (open(path, O_RDONLY) != -1)
 		return (ret_path(path, words, 0));
 	else
 	{
